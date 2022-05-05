4b8b84fc1df64a356b7158a74cd752ad51de360e
yon3030 <jason1025yon@gmail.com>
2022-05-05 17:38:34 +0200

    updating 0.7

--- pages/index.tsx
+++ pages/index.tsx
@@ -13,59 +13,61 @@ const Home: NextPage = () => {
         description="You need a website for your startup or business?"
       />
       <Header />
-      <main className="mx-auto max-w-7xl p-5">
-        <h1 className="text-xl font-bold">Selected projects.</h1>
-        <div>
-          <div className="grid gap-x-6 lg:grid-cols-2">
-            <Clients
-              title="tofanconsult.de"
-              description="NextJS, Sass, Tailwind, Framer motion, Prismic"
-              link="https://tofanconsult.de"
-              createdYear={2022}
-              projectNumber={1}
-              image="/tofanwebsite.png"
-              bgColor="first"
-            />
-            <Clients
-              title="teemone.de"
-              description="Wordpress, Sass, Gsap"
-              link="https://teemone.de"
-              createdYear={2021}
-              projectNumber={2}
-              image="/teemwebsite.png"
-              bgColor="second"
-            />
-            <Clients
-              title="vadimghedreutan.io"
-              description="NextJS, Tailwind, Sanity"
-              link="https://github.com/vadimghedreutan/VGProfile2.2"
-              createdYear={2022}
-              projectNumber={3}
-              image="/personalwebsite.png"
-              bgColor="third"
-            />
-            <Clients
-              title="fitnesstime-pf.de"
-              description="Design - Figma"
-              link="https://fitnesstime-pf.de"
-              createdYear={2021}
-              projectNumber={4}
-              image="/fitnesstimewebsite.png"
-              bgColor="fourth"
-            />
+      <main>
+        <div className="mx-auto max-w-7xl p-5">
+          <h1 className="text-xl font-bold">Selected projects.</h1>
+          <div>
+            <div className="grid gap-x-6 lg:grid-cols-2">
+              <Clients
+                title="tofanconsult.de"
+                description="NextJS, Sass, Tailwind, Framer motion, Prismic"
+                link="https://tofanconsult.de"
+                createdYear={2022}
+                projectNumber={1}
+                image="/tofanwebsite.png"
+                bgColor="first"
+              />
+              <Clients
+                title="teemone.de"
+                description="Wordpress, Sass, Gsap"
+                link="https://teemone.de"
+                createdYear={2021}
+                projectNumber={2}
+                image="/teemwebsite.png"
+                bgColor="second"
+              />
+              <Clients
+                title="vadimghedreutan.io"
+                description="NextJS, Tailwind, Sanity"
+                link="https://github.com/vadimghedreutan/VGProfile2.2"
+                createdYear={2022}
+                projectNumber={3}
+                image="/personalwebsite.png"
+                bgColor="third"
+              />
+              <Clients
+                title="fitnesstime-pf.de"
+                description="Design - Figma"
+                link="https://fitnesstime-pf.de"
+                createdYear={2021}
+                projectNumber={4}
+                image="/fitnesstimewebsite.png"
+                bgColor="fourth"
+              />
+            </div>
           </div>
-        </div>
 
-        {/* all projects */}
-        <div className="my-10 flex justify-center">
-          <a href="/projects" className="cta space-x-2">
-            all projects. <ImArrowRight2 />
-          </a>
-        </div>
+          {/* all projects */}
+          <div className="my-10 flex justify-center">
+            <a href="/projects" className="cta space-x-2">
+              all projects. <ImArrowRight2 />
+            </a>
+          </div>
 
-        {/* Say hi */}
-        <div className="mx-auto my-5 max-w-xs sm:max-w-lg">
-          <img src="/ipad_pro.png" alt="" />
+          {/* Say hi */}
+          <div className="mx-auto my-5 max-w-xs sm:max-w-lg">
+            <img src="/ipad_pro.png" alt="" />
+          </div>
         </div>
       </main>
       <Footer />
